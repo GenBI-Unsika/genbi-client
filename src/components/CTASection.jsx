6e7d9733185447a43943b45433174bce8b722499
Rangga Mukti <r4n994mukti@gmail.com>
2025-08-26 15:47:17 +0700

    FIX layout of CTASection.jsx

--- src/components/CTASection.jsx
+++ src/components/CTASection.jsx
@@ -1,11 +1,11 @@
 const CTASection = () => {
   return (
-    <div className="relative z-10 -mb-24">
+    <div className="relative z-10 -mb-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="bg-primary-500 rounded-2xl p-8 shadow-2xl">
-          <div className="text-center">
-            <h3 className="text-2xl font-bold text-white mb-2">
-              Ingin Beasiswa GenBI Indonesia dan bergabung dengan GenBI Unsika?
+          <div className="flex justify-center items-center px-8 gap-24">
+            <h3 className="text-md font-semibold text-white">
+              Daftar Beasiswa Bank Indonesia Dan Bergabung Menjadi Anggota GenBI Unsika
             </h3>
             <button className="bg-white text-primary-500 px-6 py-3 rounded-lg hover:bg-gray-100 transition-colors font-medium">
               Daftar Sekarang
